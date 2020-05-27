b7376ce0520e7f24b41f8d937f607fd8db0af3cb
VedantParikh <35135441+Vedant-06@users.noreply.github.com>
2020-05-27 15:11:48 +0530

    Create Cycle_in_Directed_Graph.cpp

---
+++ Cycle_in_Directed_Graph.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[100];
+bool v[100] = {0};
+bool recStack[100] = {0};
+
+int dfs(int s)
+{
+    if(!v[s])
+    {
+        v[s] = true;
+        recStack[s] = true;
+    
+        for(int i=0;i<adj[s].size();i++)
+        {
+            if(!v[adj[s][i]] && dfs(adj[s][i]))
+                return true;
+            else if(recStack[s] == true)
+                return true;
+        }
+    }
+    
+    cout << s << " ";
+    recStack[s] = false;
+    return false;
+    
+}
+
+int main() 
+{
+	// your code goes here
+	
+	// Nos. of Nodes
+	int n;
+	cin >> n;
+	// Nos of Edges
+	int m;
+	cin >> m;
+	
+	for(int i=0;i<m;i++)
+	{
+	    int a,b;
+	    cin >> a >> b;
+	    adj[a].push_back(b);
+	    //adj[b].push_back(a);
+	}
+	
+	cout << '\n'<< dfs(0);
+}
